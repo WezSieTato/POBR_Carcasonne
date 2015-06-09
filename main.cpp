259aab9fb94e56cdc002b8b9499e88b1e87cb25e
WezSieTato <le0n.uo@gmail.com>
2015-06-09 17:16:14 +0200

    czarowanie na prezentacje

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include "processing.h"
 #include "cutting.h"
 #include "moments.h"
+#include <iostream>
+
+#define presentation true
+#define show( a, b )     (a) = b; \
+    if(presentation){ \
+    std::cout << "Zrobione\n"; \
+cv::waitKey(-1); \
+cv::imshow("Filtrowany",(a));}
+
 
 int main(int, char *[]) {
     std::cout << "Start ..." << std::endl;
@@ -8,24 +17,32 @@ int main(int, char *[]) {
     cv::Mat image = cv::imread("test.jpeg");
     cv::Mat prog = progowanie(image, 175, false);
 
-    prog = rankFilter(prog, 3, 8);
-    prog = rankFilter(prog, 3, 8);
-    prog = rankFilter(prog, 3, 0);
-    prog = rankFilter(prog, 3, 0);
-    prog = rankFilter(prog, 3, 0);
+    cv::imshow("Normalny",image);
+    if(presentation){
+        std::cout << "Zrobione\n"; \
+        cv::imshow("Filtrowany",prog);
+        cv::waitKey(-1);
+    }
+
+    show(prog, rankFilter(prog, 3, 8));
+    show(prog, rankFilter(prog, 3, 8));
+    show(prog, rankFilter(prog, 3, 0));
+    show(prog, rankFilter(prog, 3, 0));
+    show(prog, rankFilter(prog, 3, 0));
 
     MatBoxList list = partitionBoxes(prog);
 
     int i = 0;
     for( MatBox mat : list){
-        cv::imshow(std::to_string(i), mat.first);
+        if(presentation)
+            cv::imshow(std::to_string(i), mat.first);
         ++i;
         MatInfo matInfo = doTheMath(mat);
         printMatInfo(matInfo);
     }
 
     cv::imshow("Filtrowany",prog);
-    cv::imshow("Normalny",image);
+
 
 //    cv::imwrite("prog.jpeg", max);
 //    cv::namedWindow("Max", cv::WINDOW_NORMAL);
